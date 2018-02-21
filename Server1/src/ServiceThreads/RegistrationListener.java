a4e349105153581778f2de57bb1f1c709b1575ff
FeeLiN <kostritskiysp@gmail.com>
2018-02-21 10:47:41 +0400

    12

---
+++ Server1/src/ServiceThreads/RegistrationListener.java
@@ -0,0 +1,8 @@
+package ServiceThreads;
+
+/**
+ * Created by Sergio on 17.02.2018.
+ */
+public interface RegistrationListener {
+    public void addRegisteredUser(String login, String password);
+}
